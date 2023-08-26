d73883ef26ef151897ed0d40f5dc4042fcf72404
Antonio <antoniosimeonov7@gmail.com>
2023-08-26 13:20:04 +0300

    Fix: logo bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import logo from '/logo.png';
+
 const Header = () => {
   return (
     <header className='bg-gray-700 text-white shadow-lg'>
-      <div className='container mx-auto px-4 py-4 flex items-center justify-between'>
+      <div className='container mx-auto px-4 py-4 flex items-center '>
+        <img className='rounded-full' src={logo} width={100} height={100} />
         <h1 className='text-2xl text-gray-100 font-bold p-4'>
           Crypto Pair Detective
         </h1>
